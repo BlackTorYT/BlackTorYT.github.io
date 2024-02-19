9a57a4017c515ca5db400712d9147c4d1b4f2c5a
Ilya <2003-smirnov-ilya@mail.ru>
2024-02-19 21:40:37 +1000

    release 2.6

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ burgerBtn.addEventListener('click', function() {
     burgerMenu.style.display = 'none';
   }
 });
+
+
+const fifthNavItem = document.querySelector('.nav__item:nth-child(5)'); 
+
+fifthNavItem.addEventListener('mouseenter', function() {
+  const popupMenu = this.querySelector('.nav__popup');
+  popupMenu.style.display = 'block';
+});
+
+fifthNavItem.addEventListener('mouseleave', function() {
+  const popupMenu = this.querySelector('.nav__popup');
+  popupMenu.style.display = 'none';
+});
+
+
+
+const headerArrowText = document.getElementById('burger__text-arrow');
+const burgerPopup = document.querySelector('.nav__popup-mobile');
+
+headerArrowText.addEventListener('click', function() {
+  if (burgerPopup.style.display === 'none' || burgerPopup.style.display === '') {
+    burgerPopup.style.display = 'block';
+  } else {
+    burgerPopup.style.display = 'none';
+  }
+});
+
+const headerArrow = document.getElementById('header__arrow');
+const burgerTextArrow = document.getElementById('burger__text-arrow');
+
+burgerTextArrow.addEventListener('click', function() {
+  headerArrow.classList.toggle('rotated');
+});
